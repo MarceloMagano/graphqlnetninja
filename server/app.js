2f3505f13478f48a051a2f0df4b9760c90ee7908
Marcelo Magano <marcelo.magano99@gmail.com>
2018-05-31 10:40:43 +0100

    List all books

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 const config = require('./config')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 mongoose.connect(config.db)
 mongoose.connection.once('open', () => {
