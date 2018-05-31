b7923bf398c9fb9194a669cafd53e6a782b7d6f5
Marcelo Magano <marcelo.magano99@gmail.com>
2018-05-31 19:26:57 +0100

    client done

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -2,7 +2,7 @@ import { gql } from 'apollo-boost'
 
 const getAuthorsQuery = gql`
   {
-    authors{
+    authors {
       name
       id
     }
@@ -11,11 +11,39 @@ const getAuthorsQuery = gql`
 
 const getBooksQuery = gql`
   {
-    books{
+    books {
       name
       id
     }
   }
 `
 
-export { getAuthorsQuery, getBooksQuery }
+const addBookMutation = gql`
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      name
+      id
+    }
+  }
+`
+
+const getBookQuery = gql`
+  query($id: ID) {
+    book(id: $id) {
+      id
+      name
+      genre
+      author {
+        id
+        name
+        age
+        books {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation, getBookQuery }
